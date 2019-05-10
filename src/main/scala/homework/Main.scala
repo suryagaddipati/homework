571b14766b06c22f57687bc53103806149a17016
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-05-10 18:25:54 -0500

    Test passing

--- src/main/scala/homework/Main.scala
+++ src/main/scala/homework/Main.scala
@@ -2,11 +2,18 @@ package homework
 import java.util.Calendar
 object Main extends App {
   println("Hello")
-  sealed trait View
-  object Views {
-    object Three extends View {} //sorted by last name, descending.
-    object Two extends View {} //sorted by last name, descending.
+  sealed trait View {
+    def sort(records: Seq[Record]): Seq[Record]
   }
-  def sort(records: Seq[Record], v: View): Seq[Record] = records.sortBy(_.lastName)(Ordering.by((_: String).size).reverse)
+  object Views {
+    object Two extends View { //sorted by birth date, ascending
+      def sort(records: Seq[Record]): Seq[Record] = records.sortBy(_.dateOfBirth)
+    }
+    object Three extends View { //sorted by last name, descending.
+      def sort(records: Seq[Record]): Seq[Record] = records.sortBy(_.lastName)(Ordering.by((_: String).size).reverse)
+    }
+  }
+  // def sort(records: Seq[Record], v: View): Seq[Record] =
+  def sort(records: Seq[Record], v: View): Seq[Record] = v.sort(records)
   case class Record(lastName: String, firstName: String, gender: Char, favoriteColor: String, dateOfBirth: Calendar)
 }
