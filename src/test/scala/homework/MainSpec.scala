86dfa88d6664bdbda7b00b9ce75fc673643c3d8a
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-05-10 17:56:32 -0500

    Better spec formatting

--- src/test/scala/homework/MainSpec.scala
+++ src/test/scala/homework/MainSpec.scala
@@ -21,19 +21,32 @@ class MainSpec extends FunSpec with Matchers {
       dateOfBirth
     )
   val recordsGen = Gen.listOf(recordGen)
+  def formatDate(date: java.util.Calendar): String = {
+
+    import java.time.format.DateTimeFormatter
+    import java.util.Locale
+
+    import java.time._
+    import java.time.format._
+    val df = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+
+    val zone = ZoneId.systemDefault();
+    df.format(LocalDateTime.ofInstant(date.toInstant, zone))
+  }
   describe("Sorting") {
     it("Sorts by view 3") {
       forAll(recordsGen) { (records: List[Main.Record]) =>
         val sortedByLastNameDesc = Main.sort(records, Main.Views.Three)
-        val lastNames = sortedByLastNameDesc .map(_.lastName)
+        val lastNames = sortedByLastNameDesc.map(_.lastName)
         lastNames shouldBe lastNames.sorted(Ordering.by((_: String).size).reverse)
       }
     }
     it("Sorts by view 2") {
+
       forAll(recordsGen) { (records: List[Main.Record]) =>
         val sortedByDateAsc = Main.sort(records, Main.Views.Two)
-        val dataOfBirthsSorted = sortedByDateAsc .map(_.dateOfBirth)
-        dataOfBirthsSorted  shouldBe dataOfBirthsSorted.sorted
+        val dataOfBirthsSorted = sortedByDateAsc.map(_.dateOfBirth)
+        dataOfBirthsSorted.map(formatDate) shouldBe dataOfBirthsSorted.sorted.map(formatDate)
       }
     }
   }
