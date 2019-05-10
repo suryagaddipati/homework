7fe22bad1c132f5556a3f5ed271059d8eaef4cab
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-05-10 17:06:38 -0500

    Spec passing for sorting for rule 3

--- src/main/scala/homework/Main.scala
+++ src/main/scala/homework/Main.scala
@@ -6,6 +6,6 @@ object Main extends App {
   object Views {
     object Three extends View {} //sorted by last name, descending.
   }
-  def sort(records: Seq[Record], v: View): Seq[Record] = records
+  def sort(records: Seq[Record], v: View): Seq[Record] = records.sortBy(_.lastName)(Ordering.by((_: String).size).reverse)
   case class Record(lastName: String, firstName: String, gender: Char, favoriteColor: String, dateOfBirth: Calendar)
 }
