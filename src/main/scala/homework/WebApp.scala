3671437355610a6839c19f0005375537b3fe91db
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-05-12 10:15:51 -0500

    Add failing specs for webapp

--- src/main/scala/homework/WebApp.scala
+++ src/main/scala/homework/WebApp.scala
@@ -3,15 +3,17 @@ package homework
 import org.scalatra._
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
+import scala.collection.mutable.ListBuffer
+import Cli._
+
 
 class WebApp extends ScalatraServlet with JacksonJsonSupport {
 
   before() {
     contentType = formats("json")
   }
-
-  get("/") {
-    "hello"
+val records =  new ListBuffer[Cli.Record]()
+ post("/records") {
   }
 
   protected implicit val jsonFormats: Formats = DefaultFormats
---
+++ src/test/scala/homework/WebAppSpec.scala
@@ -0,0 +1,30 @@
+package homework
+import org.scalatra.test.scalatest._
+import org.scalatest.FunSuiteLike
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import scala.collection.mutable.ListBuffer
+
+class WebAppSpec extends ScalatraSuite with FunSuiteLike {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+  addServlet(classOf[WebApp], "/*")
+
+  test("posting records") {
+    post("/records", "") {
+      status should equal(200)
+    }
+
+    post("/records", "Meow | Chairman | M | Red | 12/26/1893") {
+
+      get("/records/gender") {
+
+        status should equal(200)
+        val parsedBody = parse(body)
+        val records = parsedBody.extract[ListBuffer[Cli.Record]]
+        records.size should equal(1)
+      }
+    }
+
+  }
+}
